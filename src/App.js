839c0b758a1b460dfedf3fa4b0d2235c11bcf684
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-06-03 13:00:23 +0300

    First commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import SignInSide from './components/Home';
+import NavigationBar from './components/Navigationbar';
+
+function App() {
+  return (
+  <>
+  <NavigationBar/>
+  <SignInSide/>
+  </>
+  );
+}
+
+export default App;
