0df57ad6f6003828686c167fe2f6884608e6c663
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-07-10 22:33:23 +0300

    Added skills page

--- src/components/SKill.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-function SKill({title}) {
-  return (
-    <div className='px-3 me-2 font-medium rounded-full border-2 border-gray-500 bg-gray-200'>{title}</div>
-  )
-}
-
-export default SKill
--- src/pages/Platform/Users/Skills.js
+++ src/pages/Platform/Users/Skills.js
@@ -1,70 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import Cookies from "universal-cookie";
-import SKill from "../../../components/SKill";
+import Select from "react-select";
+import "flowbite/dist/flowbite.min.js";
+import { useNavigate } from "react-router-dom";
 
 function Skills() {
   const cookie = new Cookies();
+  const [selectedSkills, setSelectedSkills] = useState();
+  const [selectedProfession, setSelectedProfession] = useState();
+  const navigate = useNavigate()
+
+  const skillList = [
+    { value: "red", label: "Red" },
+    { value: "green", label: "Green" },
+    { value: "yellow", label: "Yellow" },
+    { value: "blue", label: "Blue" },
+    { value: "white", label: "White" }
+  ];
+  const professionList = [
+    { value: "medicine", label: "Medicine" },
+    { value: "software developer", label: "Software Developer" },
+    { value: "law", label: "Law" },
+    { value: "Accounting and Finance", label: "Accounting & Finance" },
+    { value: "civil engineering", label: "Civil Engineering" }
+  ];
+
+  const submitHandler = () => {
+        navigate("/jobs")
+  }
   return (
-    <div className="p-10 my-12 mx-[200px] grid place-items-center align-center rounded overflow-hidden shadow-lg h-min-[70vh]">
-      <form className="w-full">
-        <div class="mb-6">
+    <div className="p-10 lg:my-12 lg:mx-[200px] sm:m-5 md:m-10 grid place-items-center align-center rounded overflow-hidden shadow-lg h-min-[70vh]">
+      <form onSubmit={submitHandler} className="w-full">
+        <div className="mb-6">
           <label
-            for="email"
-            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-full"
+            for="profession"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-full"
           >
-            Your email
+            Profession
           </label>
-          <input
-            type="email"
-            id="email"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@flowbite.com"
-            required
-          />
+          <Select
+        className="bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        options={professionList}
+        placeholder="Medicine, technology,....."
+        onChange={(val) => {
+            setSelectedProfession(val)
+        }}
+        isSearchable={true}
+        value={selectedProfession}
+        />
         </div>
-        <div class="mb-6">
+        <div className="mb-6">
           <label
-            for="password"
+            for="About you"
             class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           >
-            Your password
+        About you
           </label>
-          <input
-            type="password"
-            id="password"
-            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            required
-          />
+          <textarea
+            id="about"
+            rows="4"
+            class="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+            placeholder="Describe yourself,..."
+          ></textarea>
         </div>
         <label
-        for="countries"
-        class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-      >
-        Select your country
-      </label>
-      <select
-        id="countries"
-        class="bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      >
-        <option>United States</option>
-        <option>Canada</option>
-        <option>France</option>
-        <option>Germany</option>
-      </select>
-      <div className="flex mb-3">
-            <SKill title="HTML"/>
-            <SKill title="HTML"/>
-            <SKill title="HTML"/>
-            <SKill title="HTML"/>
-      </div>
+          for="skills"
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+        >
+          Select your prominent skills (6 max).
+        </label>
+        <Select
+        className="bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        options={skillList}
+        placeholder="Select skills,..."
+        onChange={(val) => {
+            if(val.length <= 6) {
+                setSelectedSkills(val)
+            } else {
+                alert("Max number of skills added!")
+            }
+        }}
+        isSearchable={true}
+        value={selectedSkills}
+        isMulti
+        />
+
+<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="file_input">Upload you Resume</label>
+<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="file_input" accept=".pdf,.doc,.docx" type="file"/>
+
         <button
           type="submit"
-          class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="mt-3 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Submit
         </button>
       </form>
-
     </div>
   );
 }
