1cfd65547aafc5ce2f7a810bc9e2ba64221ef57b
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-07-12 20:10:25 +0300

    Added profile image selection

--- src/pages/Platform/Companies/CompanyDetails.js
+++ src/pages/Platform/Companies/CompanyDetails.js
@@ -4,6 +4,7 @@ import Select from "react-select";
 import "flowbite/dist/flowbite.min.js";
 import { useNavigate } from "react-router-dom";
 import { Typography } from "@mui/material";
+import { industries } from "../../../Data/CompanyIndustries";
 
 function CompanyDetails() {
   const cookie = new Cookies();
@@ -11,20 +12,6 @@ function CompanyDetails() {
   const [selectedProfession, setSelectedProfession] = useState();
   const navigate = useNavigate();
 
-  const skillList = [
-    { value: "red", label: "Red" },
-    { value: "green", label: "Green" },
-    { value: "yellow", label: "Yellow" },
-    { value: "blue", label: "Blue" },
-    { value: "white", label: "White" },
-  ];
-  const professionList = [
-    { value: "medicine", label: "Medicine" },
-    { value: "software developer", label: "Software Developer" },
-    { value: "law", label: "Law" },
-    { value: "Accounting and Finance", label: "Accounting & Finance" },
-    { value: "civil engineering", label: "Civil Engineering" },
-  ];
 
   const submitHandler = () => {
     navigate("/company-jobs");
@@ -33,16 +20,30 @@ function CompanyDetails() {
     <div className="p-10 lg:my-12 lg:mx-[200px] sm:m-5 md:m-10 grid place-items-center align-center rounded overflow-hidden shadow-lg h-min-[70vh]">
       <form onSubmit={submitHandler} className="w-full">
         <div className="mb-6">
+
+<div class="flex items-center justify-center w-full">
+    <label for="dropzone-file" class="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600">
+        <div class="flex flex-col items-center justify-center pt-5 pb-6">
+            <svg class="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 16">
+                <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"/>
+            </svg>
+            <p class="mb-2 text-sm text-gray-500 dark:text-gray-400"><span class="font-semibold">Click to upload</span> or drag and drop</p>
+            <p class="text-xs text-gray-500 dark:text-gray-400">JPEG, PNG, or JPG</p>
+        </div>
+    </label>
+</div> 
+<input id="dropzone-file" type="file" accept=".jpeg, .png, .jpg" class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"  />
+
           <label
             for="profession"
-            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-full"
+            className="block mb-2 mt-3 text-sm font-medium text-gray-900 dark:text-white w-full"
           >
             Company Type
           </label>
           <Select
             className="bg-gray-50 border mb-4 border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            options={professionList}
-            placeholder="Medicine, technology,....."
+            options={industries}
+            placeholder="Healthcare, technology,....."
             onChange={(val) => {
               setSelectedProfession(val);
             }}
@@ -65,19 +66,22 @@ function CompanyDetails() {
           ></textarea>
         </div>
         <label
+            class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Is your company licensed?
+          </label>
+<div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
+    <input id="bordered-radio-1" type="radio" value="Yes" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+    <label for="bordered-radio-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Yes</label>
+</div>
+<div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700 mb-5">
+    <input checked id="bordered-radio-2" type="radio" value="No" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
+    <label for="bordered-radio-2" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">No</label>
+</div>
+<label
           class="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
           for="file_input"
         >
-        <Typography variant="label">Do you have a trading license?</Typography><br/>
-
-<div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
-    <input id="bordered-radio-1" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label for="bordered-radio-1" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Default radio</label>
-</div>
-<div class="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700 mb-5">
-    <input checked id="bordered-radio-2" type="radio" value="" name="bordered-radio" class="w-4 h-4 text-blue-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"/>
-    <label for="bordered-radio-2" class="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300">Checked state</label>
-</div>
 
         </label>
         <input
