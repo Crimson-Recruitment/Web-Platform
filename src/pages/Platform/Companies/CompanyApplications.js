6e7ec4292bce156198efa92a32f1beb71c2454f7
BenziDarwin <ssalibenjamin0402@gmail.com>
2023-07-16 15:24:57 +0300

    Edited Company applications

--- src/pages/Platform/Companies/CompanyApplications.js
+++ src/pages/Platform/Companies/CompanyApplications.js
@@ -1,94 +1,20 @@
 import React from "react";
 import CompanySideBar from "../../../components/CompanySideBar";
 import { MDBCardBody, MDBCardTitle, MDBCardText, MDBCard } from "mdbreact";
+import CompanyApplicationCard from "../../../components/CompanyApplicationCard";
+import "flowbite/dist/flowbite.min.js";
 
 function CompanyApplications() {
   return (
     <CompanySideBar>
-      <MDBCard className="my-3 me-3 w-75">
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </MDBCardText>
-          <MDBCardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
-      <MDBCard className="my-3 me-3 w-75">
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </MDBCardText>
-          <MDBCardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
-      <MDBCard className="my-3 me-3 w-75">
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </MDBCardText>
-          <MDBCardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
-      <MDBCard className="my-3 me-3 w-75">
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </MDBCardText>
-          <MDBCardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
-      <MDBCard className="my-3 me-3 w-75">
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </MDBCardText>
-          <MDBCardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
-      <MDBCard className="my-3 me-3 w-75">
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </MDBCardText>
-          <MDBCardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
-      <MDBCard className="my-3 me-3 w-75">
-        <MDBCardBody>
-          <MDBCardTitle>Card title</MDBCardTitle>
-          <MDBCardText>
-            This is a wider card with supporting text below as a natural lead-in
-            to additional content. This content is a little bit longer.
-          </MDBCardText>
-          <MDBCardText>
-            <small className="text-muted">Last updated 3 mins ago</small>
-          </MDBCardText>
-        </MDBCardBody>
-      </MDBCard>
+   <div type="button" data-drawer-target="drawer-swipe" data-drawer-show="drawer-swipe" data-drawer-placement="bottom" data-drawer-edge="true" data-drawer-edge-offset="bottom-[60px]" aria-controls="drawer-swipe">
+   <CompanyApplicationCard applicant={"Ssali Benjamin"} timeOfApplication={new Date().toDateString()} jobName={"Software Engineer"} applicationStatus={"Reviewing"}/>
+  </div>
+
+  <div id="drawer-swipe" class="fixed  h-[60vh] z-40 w-full overflow-y-auto bg-white border-t border-gray-200 rounded-t-lg dark:border-gray-700 dark:bg-gray-800 transition-transform bottom-0 left-0 right-0 translate-y-full bottom-[60px]" tabindex="-1" aria-labelledby="drawer-swipe-label">
+  
+   </div>
+
     </CompanySideBar>
   );
 }
