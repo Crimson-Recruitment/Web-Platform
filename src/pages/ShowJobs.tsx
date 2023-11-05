26c4214ba1fb0033071e30f970486a7905cb2901
Benzi Darwin <ssalibenjamin0402@gmail.com>
2023-11-05 23:07:38 +0300

    Added pagination

--- src/pages/ShowJobs.tsx
+++ src/pages/ShowJobs.tsx
@@ -15,20 +15,38 @@ import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import Link from '@mui/material/Link';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
+import { Pagination } from '@mui/material';
 
-const cards = [1, 2, 3, 4, 5, 6,7,8,9,10,11,12];
+const cards = [1, 2, 3, 4, 5, 6,7,8,9,10,11];
+const pageSize = 6;
 
 // TODO remove, this demo shouldn't need to reset the theme.
 const defaultTheme = createTheme();
 
 export default function ShowJobs() {
+  const [pagination, setPagination] = React.useState({from:0, to:0});
+  const pages = Math.ceil(cards.length/pageSize);
+  const [index, setIndex] = React.useState(0);
+  React.useEffect(() => {
+    if(index+1 < pages) {
+      setPagination({from:index*pageSize, to:pageSize})
+    } else {
+      setPagination({from:index*pageSize, to:cards.length})
+    }
+    
+  }, [index])
+
+  const pageHandler = (event:React.ChangeEvent<unknown>, page:number) => {
+    setIndex(page-1);
+  }
+
   return (
     <ThemeProvider theme={defaultTheme}>
       <main>
         <Container sx={{ py: 8 }} maxWidth="xl">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
+            {cards.slice(pagination.from,pagination.to).map((card) => (
               <Grid item key={card} xs={12} sm={6} md={4}>
                 <Card
                   sx={{ height: '100%', display: 'flex', flexDirection: 'column' }}
@@ -49,6 +67,18 @@ export default function ShowJobs() {
                 </Card>
               </Grid>
             ))}
+          </Grid>
+          <Grid 
+          sx={{    
+            display: "flex",
+            flexDirection: "column",
+            alignItems: "center",
+            marginTop:"20px"
+            }}>
+          <Pagination
+          count={Math.ceil(cards.length/6)}
+            onChange={pageHandler}
+          />
           </Grid>
         </Container>
       </main>
