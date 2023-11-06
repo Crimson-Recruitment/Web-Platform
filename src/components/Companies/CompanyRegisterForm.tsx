6db0936dd0a12cfbf611d4f3f8d9b6eafe145c2a
Benzi Darwin <ssalibenjamin0402@gmail.com>
2023-11-07 00:45:27 +0300

    Added all fields for register

--- src/components/Companies/CompanyRegisterForm.tsx
+++ src/components/Companies/CompanyRegisterForm.tsx
@@ -9,12 +9,12 @@ import { Link } from 'react-router-dom';
 import { SubmitHandler, useForm } from "react-hook-form";
 import { object, string, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Grid, TextField } from '@mui/material';
+import { Avatar, Grid, TextField } from '@mui/material';
 import LocationSearchInput from '../LocationInput';
 import MuiPhoneNumber from 'material-ui-phone-number';
 
 
- const steps = ['Contact info', 'Company details'];
+ const steps = ['Contact info', 'Company details', "Profile Image"];
 
 export default function CompanyRegisterForm() {
   const [activeStep, setActiveStep] = React.useState(0);
@@ -198,10 +198,117 @@ export default function CompanyRegisterForm() {
                  </Grid>
                  
                </Box>
-             :
-          <Typography sx={{ mt: 2, mb: 1 }}>
-            Bye!
-            </Typography>}
+             : activeStep == 1 ? 
+             <>
+        <div className="mb-6">
+          <label
+            htmlFor="overview"
+            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+          >
+            Company Overview
+          </label>
+          <textarea
+            required
+            id="overview"
+            name="overview"
+            rows={4}
+            className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+            placeholder="About the company"
+          ></textarea>
+        </div>
+        <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+          Is your company licensed?
+        </label>
+        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
+          <input
+            required
+            checked
+            id="bordered-radio-1"
+            type="radio"
+            value={"true"}
+            name="bordered-radio"
+            className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="bordered-radio-1"
+            className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            Yes
+          </label>
+        </div>
+        <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700 mb-5">
+          <input
+            required
+            id="bordered-radio-2"
+            type="radio"
+            value={"false"}
+            name="bordered-radio"
+            className="w-4 h-4 text-green-600 bg-gray-100 border-gray-300 focus:ring-green-500 dark:focus:ring-green-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
+          />
+          <label
+            htmlFor="bordered-radio-2"
+            className="w-full py-4 ml-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+          >
+            No
+          </label>
+        </div>
+
+             </>:
+          <>
+            <label
+            htmlFor="dropzone-file"
+            className="block mb-2 mt-3 text-sm font-medium text-gray-900 dark:text-white w-full"
+          >
+            Company Logo
+          </label>
+          {false ? (
+            <div className="flex justify-center mb-5">
+              <Avatar
+                alt="Logo"
+                sx={{ height: "400px", width: "400px" }}
+              />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center w-full">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or drag
+                    and drop Company Logo.
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    JPEG, PNG, or JPG
+                  </p>
+                </div>
+              </label>
+            </div>
+          )}
+          <input
+            required
+            id="dropzone-file"
+            type="file"
+            accept=".jpeg, .png, .jpg"
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          />
+          </>}
           <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
             <Button
               color="inherit"
--- src/components/Users/UserRegisterForm.tsx
+++ src/components/Users/UserRegisterForm.tsx
@@ -8,13 +8,14 @@ import Typography from '@mui/material/Typography';
 import { SubmitHandler, useForm } from "react-hook-form";
 import { object, string, z } from "zod";
 import { zodResolver } from "@hookform/resolvers/zod";
-import { Grid, TextField } from '@mui/material';
+import { Avatar, Grid, Select, TextField } from '@mui/material';
 import LocationSearchInput from '../LocationInput';
 import MuiPhoneNumber from 'material-ui-phone-number';
 import { Link } from 'react-router-dom';
+import { professionList } from '../../Data/UserProfessions';
 
 
-const steps = ['Contact info', 'User details'];
+const steps = ['Contact info', 'User details', "Profile Image"];
 
 export default function UserRegisterForm() {
   const [activeStep, setActiveStep] = React.useState(0);
@@ -200,10 +201,104 @@ export default function UserRegisterForm() {
                </Grid>
                
              </Box>
-           :
-        <Typography sx={{ mt: 2, mb: 1 }}>
-          Bye!
-          </Typography>}
+           : activeStep == 1
+           ? <form className="w-full">
+           <div className="mb-6">
+             <label
+               htmlFor="profession"
+               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white w-full"
+             >
+               Profession
+             </label>
+           </div>
+           <div className="mb-6">
+             <label
+               htmlFor="about"
+               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+             >
+               About you
+             </label>
+             <textarea
+               required
+               id="about"
+               rows={4}
+               name="about"
+               className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-green-500 focus:border-green-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-green-500 dark:focus:border-green-500"
+               placeholder="Describe yourself,..."
+             ></textarea>
+           </div>
+           <label
+             htmlFor="skills"
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+           >
+             Select your prominent skills (6 max).
+           </label>
+   
+           <label
+             className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+             htmlFor="file_input"
+           >
+             Upload you Resume (max size 2mbs)
+           </label>
+           <input
+             required
+             className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+             id="file_input"
+             accept=".pdf,.doc,.docx"
+             type="file"
+           />
+         </form> :
+        <>
+         {false ? (
+            <div className="flex justify-center mb-5">
+              <Avatar
+                alt="Logo"
+                sx={{ height: "400px", width: "400px" }}
+                
+              />
+            </div>
+          ) : (
+            <div className="flex items-center justify-center w-full">
+              <label
+                htmlFor="dropzone-file"
+                className="flex flex-col items-center justify-center w-full h-64 border-2 border-gray-300 border-dashed rounded-lg cursor-pointer bg-gray-50 dark:hover:bg-bray-800 dark:bg-gray-700 hover:bg-gray-100 dark:border-gray-600 dark:hover:border-gray-500 dark:hover:bg-gray-600"
+              >
+                <div className="flex flex-col items-center justify-center pt-5 pb-6">
+                  <svg
+                    className="w-8 h-8 mb-4 text-gray-500 dark:text-gray-400"
+                    aria-hidden="true"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 20 16"
+                  >
+                    <path
+                      stroke="currentColor"
+                      stroke-linecap="round"
+                      stroke-linejoin="round"
+                      stroke-width="2"
+                      d="M13 13h3a3 3 0 0 0 0-6h-.025A5.56 5.56 0 0 0 16 6.5 5.5 5.5 0 0 0 5.207 5.021C5.137 5.017 5.071 5 5 5a4 4 0 0 0 0 8h2.167M10 15V6m0 0L8 8m2-2 2 2"
+                    />
+                  </svg>
+                  <p className="mb-2 text-sm text-gray-500 dark:text-gray-400">
+                    <span className="font-semibold">Click to upload</span> or drag
+                    and drop
+                  </p>
+                  <p className="text-xs text-gray-500 dark:text-gray-400">
+                    JPEG, PNG, or JPG (3mbs max)
+                  </p>
+                </div>
+              </label>
+            </div>
+          )}
+          <input
+            required
+            id="dropzone-file"
+            type="file"
+            accept=".jpeg, .png, .jpg"
+            className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+          />
+
+        </>}
         <Box sx={{ display: 'flex', flexDirection: 'row', pt: 2 }}>
           <Button
             color="inherit"
