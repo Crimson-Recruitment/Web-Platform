2f6ed01b6dde322ed1e87c15adc60fed1c991064
Benzi Darwin <ssalibenjamin0402@gmail.com>
2023-11-14 23:28:42 +0300

    Started working on company profile page

--- src/pages/Platform/Companies/CompanyProfile.tsx
+++ src/pages/Platform/Companies/CompanyProfile.tsx
@@ -10,154 +10,118 @@ import IconButton from "@mui/material/IconButton";
 import Avatar from "@mui/material/Avatar";
 import Typography from "@mui/material/Typography";
 import Box from "@mui/material/Box";
+import { CardMedia } from "@mui/material";
 
 const CompanyProfile = () => {
   return (
-    <div className="gradient-custom-2" style={{ backgroundColor: "#9de2ff" }}>
-      <Container className="py-5 h-100">
-        <Grid
-          container
-          justifyContent="center"
-          alignItems="center"
-          className="h-100"
-        >
-          <Grid item lg={9} xl={7}>
-            <Card>
-              <div
-                className="rounded-top text-white d-flex flex-row"
-                style={{ backgroundColor: "#000", height: "200px" }}
-              >
-                <Box
-                  sx={{
-                    ms: 4,
-                    mt: 5,
-                    width: "150px",
-                    display: "flex",
-                    flexDirection: "column",
-                  }}
-                >
-                  <Avatar
-                    src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
-                    alt="Generic placeholder image"
-                    className="mt-4 mb-2 img-thumbnail"
-                    sx={{ width: "150px", zIndex: "1" }}
+    <div className="gradient-custom-2">
+         <Container className="py-5 h-100">
+      <Grid container justifyContent="center" alignItems="center" className="h-100">
+        <Grid item lg={9} xl={7}>
+          <Card>
+            <Box sx={{ bgcolor: '#000', height: '200px', display: 'flex', flexDirection: 'row', alignItems: 'center', justifyContent: 'space-between', padding: '16px' }}>
+              <Box sx={{ display: 'flex', flexDirection: 'column', width: '150px' }}>
+                <CardMedia
+                  component="img"
+                  alt="Profile Image"
+                  height="150"
+                  image="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-profiles/avatar-1.webp"
+                  style={{ width: '150px', zIndex: '1' }}
+                />
+                <Button variant="outlined" style={{ height: '36px', overflow: 'visible', marginTop: '8px' }}>
+                  Edit profile
+                </Button>
+              </Box>
+              <Box sx={{ marginLeft: '16px', marginTop: '16px' }}>
+                <Typography variant="h5" color="textPrimary">
+                  Andy Horwitz
+                </Typography>
+                <Typography variant="body1" color="textSecondary">
+                  New York
+                </Typography>
+              </Box>
+            </Box>
+            <Box sx={{ bgcolor: '#f8f9fa', padding: '16px' }}>
+              <Box sx={{ display: 'flex', justifyContent: 'end', textAlign: 'center', py: 1 }}>
+                <Box>
+                  <Typography variant="h5" className="mb-1">
+                    253
+                  </Typography>
+                  <Typography variant="body2" className="small text-muted mb-0">
+                    Photos
+                  </Typography>
+                </Box>
+                <Box sx={{ px: 3 }}>
+                  <Typography variant="h5" className="mb-1">
+                    1026
+                  </Typography>
+                  <Typography variant="body2" className="small text-muted mb-0">
+                    Followers
+                  </Typography>
+                </Box>
+                <Box>
+                  <Typography variant="h5" className="mb-1">
+                    478
+                  </Typography>
+                  <Typography variant="body2" className="small text-muted mb-0">
+                    Following
+                  </Typography>
+                </Box>
+              </Box>
+            </Box>
+            <CardContent className="text-black p-4">
+              <Box sx={{ mb: 5 }}>
+                <Typography variant="h6" className="fw-normal mb-1">
+                  About
+                </Typography>
+                <Box sx={{ bgcolor: '#f8f9fa', p: 4 }}>
+                  <Typography variant="body1" className="font-italic mb-1">
+                    Web Developer
+                  </Typography>
+                  <Typography variant="body1" className="font-italic mb-1">
+                    Lives in New York
+                  </Typography>
+                  <Typography variant="body1" className="font-italic mb-0">
+                    Photographer
+                  </Typography>
+                </Box>
+              </Box>
+              <Box sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center', mb: 4 }}>
+                <Typography variant="h6" className="fw-normal mb-0">
+                  Recent photos
+                </Typography>
+                <Typography variant="body1" className="mb-0">
+                  <a href="#!" className="text-muted">
+                    Show all
+                  </a>
+                </Typography>
+              </Box>
+              <Grid container spacing={2}>
+                <Grid item xs={6} md={6} lg={6}>
+                  <CardMedia
+                    component="img"
+                    alt="Recent Photo"
+                    height="140"
+                    image="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
+                    className="w-100 rounded-3"
                   />
-                  <Button
-                    variant="outlined"
-                    style={{ height: "36px", overflow: "visible" }}
-                  >
-                    Edit profile
-                  </Button>
-                </Box>
-                <Box sx={{ ms: 3, marginTop: "130px" }}>
-                  <Typography variant="h5">Andy Horwitz</Typography>
-                  <Typography variant="body2">New York</Typography>
-                </Box>
-              </div>
-              <div
-                className="p-4 text-black"
-                style={{ backgroundColor: "#f8f9fa" }}
-              >
-                <div className="d-flex justify-content-end text-center py-1">
-                  <div>
-                    <Typography variant="h5" className="mb-1">
-                      253
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      className="small text-muted mb-0"
-                    >
-                      Photos
-                    </Typography>
-                  </div>
-                  <Box sx={{ px: 3 }}>
-                    <Typography variant="h5" className="mb-1">
-                      1026
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      className="small text-muted mb-0"
-                    >
-                      Followers
-                    </Typography>
-                  </Box>
-                  <div>
-                    <Typography variant="h5" className="mb-1">
-                      478
-                    </Typography>
-                    <Typography
-                      variant="body2"
-                      className="small text-muted mb-0"
-                    >
-                      Following
-                    </Typography>
-                  </div>
-                </div>
-              </div>
-              <CardContent className="text-black p-4">
-                <div className="mb-5">
-                  <Typography variant="h5" className="fw-normal mb-1">
-                    About
-                  </Typography>
-                  <div className="p-4" style={{ backgroundColor: "#f8f9fa" }}>
-                    <Typography variant="body2" className="font-italic mb-1">
-                      Web Developer
-                    </Typography>
-                    <Typography variant="body2" className="font-italic mb-1">
-                      Lives in New York
-                    </Typography>
-                    <Typography variant="body2" className="font-italic mb-0">
-                      Photographer
-                    </Typography>
-                  </div>
-                </div>
-                <div className="d-flex justify-content-between align-items-center mb-4">
-                  <Typography variant="h5" className="fw-normal mb-0">
-                    Recent photos
-                  </Typography>
-                  <Typography variant="body2" className="mb-0">
-                    <a href="#!" className="text-muted">
-                      Show all
-                    </a>
-                  </Typography>
-                </div>
-                <Grid container>
-                  <Grid item className="mb-2">
-                    <Avatar
-                      src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(112).webp"
-                      alt="image 1"
-                      className="w-100 rounded-3"
-                    />
-                  </Grid>
-                  <Grid item className="mb-2">
-                    <Avatar
-                      src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
-                      alt="image 1"
-                      className="w-100 rounded-3"
-                    />
-                  </Grid>
                 </Grid>
-                <Grid container spacing={2} className="g-2">
-                  <Grid item className="mb-2">
-                    <Avatar
-                      src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(108).webp"
-                      alt="image 1"
-                      className="w-100 rounded-3"
-                    />
-                  </Grid>
-                  <Grid item className="mb-2">
-                    <Avatar
-                      src="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(114).webp"
-                      alt="image 1"
-                      className="w-100 rounded-3"
-                    />
-                  </Grid>
+                <Grid item xs={6} md={6} lg={6}>
+                  <CardMedia
+                    component="img"
+                    alt="Recent Photo"
+                    height="140"
+                    image="https://mdbcdn.b-cdn.net/img/Photos/Lightbox/Original/img%20(107).webp"
+                    className="w-100 rounded-3"
+                  />
                 </Grid>
-              </CardContent>
-            </Card>
-          </Grid>
+                {/* Add more Grid items for additional photos */}
+              </Grid>
+            </CardContent>
+          </Card>
         </Grid>
-      </Container>
+      </Grid>
+    </Container>
     </div>
   );
 };
