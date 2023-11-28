431c1f290837601d84332c89c1f4a3382acbd1fe
Benzi Darwin <ssalibenjamin0402@gmail.com>
2023-11-28 21:14:35 +0300

    Testing CORS

--- src/core/baseURL.tsx
+++ src/core/baseURL.tsx
@@ -1,8 +1,7 @@
 import axios from "axios";
 
 export const baseUrl = "https://crimson.eu-north-1.elasticbeanstalk.com/api/v1";
-// export const userURL = 'http://localhost:8082';
-// export const leaveURL = "http://localhost:8080";
+//export const baseUrl = "http://localhost:8081/api/v1";
 
 export const AxiosUserInstance = axios.create({
   headers: {
